dbae5eae54b10bc368db04fc15a7e1e32acab0f7
BuildTools <shiryu325@gmail.com>
2015-02-07 14:35:20 -0800

    feed.rb

--- r-feed.rb
+++ r-feed.rb
@@ -5,26 +5,31 @@ require 'open-uri'
 days = 2
 maxage = 60 * 60 * 24 * days
 feedfile = "/home/voltron/Prog/feeds.txt"
+poddir = "/home/voltron/pod/"
 
 File.open(feedfile).each do |line| 
 
   rss = RSS::Parser.parse(line, false)
-  puts "==========================================="
-  puts "#{rss.channel.title}"
-  puts "==========================================="
+  puts "\n==========================================="
+  puts "= #{rss.channel.title}"
 
   rss.items.each do |item|
   age = Time.now - Time.at(item.pubDate) 
 
     if age < maxage
-      puts "[#{item.pubDate}] - #{item.title}, published #{(age / 86400).round} days ago"
+      puts "\n[#{item.pubDate}] - #{item.title}, published #{(age / 86400).round} days ago."
 
-      filename = item.enclosure.url.scan(/[^\/]*.mp3/)
+      filename = item.enclosure.url.scan(/[^\/]*.mp3/).last.to_s
 
-      File.open(filename.to_s, 'wb') do |file|
-        file.write open(item.enclosure.url).read
+      if File.file?("#{poddir}/#{filename}") 
+        puts "*** #{filename} exists, skipping...\n"
+      else
+        puts "*** Downloading #{filename}\n"
+        File.open("#{poddir}/#{filename}", 'wb') do |file|
+          file.write open(item.enclosure.url).read
+        end
       end
-
     end
   end
+  puts "==========================================="
 end
