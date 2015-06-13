ce10e8131ffba98ce0ac905b72c28e3e9985d7a0
BuildTools <shiryu325@gmail.com>
2015-06-13 14:03:14 -0700

    minor update

--- lotto/ran.pl
+++ lotto/ran.pl
@@ -6,12 +6,14 @@ my @game3 = qw(1 39 43 44 47 10) ;
 
 my $drange = 47 ;
 my $mrange = 27 ;
+my $prints = 1000000;
 
+print "Draw range $drange, Mega range $mrange, printing every $prints draws:\n";
 my $i = 0  ;
 do {
-	my @game0 = pick() ;
+	my @game0 = pick($drange, $mrange) ;
 	$i++ ;
-	if ($i % 1000000 == 0) { 
+	if ($i % $prints == 0) { 
 		print "$i\t - " ;
 		printGame(@game0) ;
 	}
@@ -28,17 +30,18 @@ sub printGame {
 }
 
 sub pick {
+	my ($draw_range, $mega_range) = @_ ;
 	my @sort ;
 	my %h ;
 
 	for (my $i = 0 ; $i < 5 ; $i++ if (!$h{$x}++)) {
-		$x = int (rand($drange) + 1) ;
+		$x = int (rand($draw_range) + 1) ;
 	}
 
 	for my $x ( sort {$a<=>$b} keys %h) {
 		push @sort, $x ;
 	}
-	push @sort, int (rand($mrange) + 1) ;
+	push @sort, int (rand($mega_range) + 1) ;
 	return @sort ;
 }
 
